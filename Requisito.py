8fdaaabf58436c7d48e1eb1834c4bc176dadc784
David Montesinos <mdmontesinos@hotmail.com>
2023-04-14 18:43:31 +0200

    Definir tipos de datos en constructor

--- Requisito.py
+++ Requisito.py
@@ -1,6 +1,6 @@
 class Requisito:
 
-    def __init__(self, descripcion, coste, importancia):
+    def __init__(self, descripcion: str, coste: int, importancia: int):
         self.descripcion = descripcion
         self.coste = coste
         self.importancia = importancia
