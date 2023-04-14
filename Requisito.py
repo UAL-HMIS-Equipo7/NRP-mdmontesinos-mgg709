7e6e29bf3f130d9746b0fc4ff247ab88c05843f7
David Montesinos <mdmontesinos@hotmail.com>
2023-04-14 18:35:43 +0200

    Creación inicial clase Requisito

---
+++ Requisito.py
@@ -0,0 +1,8 @@
+class Requisito:
+
+    def __init__(self, descripcion, coste, importancia):
+        self.descripcion = descripcion
+        self.coste = coste
+        self.importancia = importancia
+
+    
